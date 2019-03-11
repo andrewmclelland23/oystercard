8cd7e64bb4532c11a3c57faea47c35abd30c7af8
alexanderchen6 <alexbaggio315@hotmail.com>
2019-03-11 14:48:15 +0000

    Set balance as 0 at instantiation and added attr_reader

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,5 +2,13 @@ require 'oystercard'
 
 describe Oystercard do 
 
+    describe '#balance' do
+    
+    it 'has a balance of 0 when initialized' do
+        expect(subject.balance).to eq 0
+    end
+
+    end
+
 
 end 
